29482de1dafb4095516f51b1e20362cc9dbf5dbf
Steven <stevenxchung@gmail.com>
2019-02-04 21:01:32 -0500

    Python basics quiz initial commit

---
+++ 01-python-basics-quiz.py
@@ -0,0 +1,18 @@
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+  sentence = 0
+  init = "I am super excited for this course!"
+  combined = ""
+  while (sentence < 5):
+    sentence += 1
+    combined += init + " "
+  return combined
+
+print(show_excitement())
